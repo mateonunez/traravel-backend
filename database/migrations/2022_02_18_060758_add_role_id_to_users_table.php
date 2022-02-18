d17880db38a49d56bedd18219f566c3d0c691bbc
mateonunez <mateonunez95@gmail.com>
2022-02-18 11:51:05 +0100

    chore(User): add roleId after user id

--- database/migrations/2022_02_18_060758_add_role_id_to_users_table.php
+++ database/migrations/2022_02_18_060758_add_role_id_to_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->uuid('roleId')->constrained('roles');
+            $table->uuid('roleId')->after('id')->constrained('roles');
         });
     }
 
