bc671054c8ebeb30e011573b76db474c20a6abac
mateonunez <mateonunez95@gmail.com>
2022-02-21 10:05:00 +0100

    chore(Mood): add emoji column

--- database/seeders/MoodSeeder.php
+++ database/seeders/MoodSeeder.php
@@ -18,36 +18,43 @@ class MoodSeeder extends Seeder
         $payloadNature = [
             'name' => 'Nature',
             'description' => 'Nature is the natural world, the natural world is the nature.',
+            'emoji' => '🌳',
         ];
 
         $payloadRelax = [
             'name' => 'Relax',
             'description' => 'Relax is the time to relax, the time to unwind.',
+            'emoji' => '💤',
         ];
 
         $payloadHistory = [
             'name' => 'History',
             'description' => 'History is the past, the past is the history.',
+            'emoji' => '📚',
         ];
 
         $payloadCulture = [
             'name' => 'Culture',
             'description' => 'Culture is the art, the art is the culture.',
+            'emoji' => '🎨',
         ];
 
         $payloadParty = [
             'name' => 'Party',
             'description' => 'Party is the fun, the fun is the party.',
+            'emoji' => '🎉',
         ];
 
         $payloadAdventure = [
             'name' => 'Adventure',
             'description' => 'Adventure is the journey, the journey is the adventure.',
+            'emoji' => '🗺️',
         ];
 
         $payloadFood = [
             'name' => 'Food',
             'description' => 'Food is the food, the food is the food.',
+            'emoji' => '🍽️',
         ];
 
         Mood::create($payloadNature);
